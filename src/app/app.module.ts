6a79800be3fd95595d5a0f71965ffa85796db89a
mohamed_sakr <sakr83117@gmail.com>
2023-10-12 21:42:49 +0300

    added firebase configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +18,10 @@ import { MaterialModule } from './material/material.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'e-learning-app-6606a',
+    appId: '1:569875074826:web:3071b3f8b71bb88afaf4f3',
+    storageBucket: 'e-learning-app-6606a.appspot.com',
+    apiKey: 'AIzaSyCswBLwLjZk_g9WTP2TgIAOAlRCzx-xA_s',
+    authDomain: 'e-learning-app-6606a.firebaseapp.com',
+    messagingSenderId: '569875074826',
+  }, production: false };
