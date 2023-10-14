a07ef1e9c0ac5ecd4734887f2b2cac25c86bf6bd
mohamed_sakr <sakr83117@gmail.com>
2023-10-14 22:31:01 +0300

    added route guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {
+  AngularFireAuthGuard,
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+} from '@angular/fire/compat/auth-guard';
+
+const guestOnly = () => redirectLoggedInTo(['dashboard']);
+const authOnly = () => redirectUnauthorizedTo(['']);
 
 const routes: Routes = [
   {
@@ -10,6 +18,15 @@ const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: guestOnly },
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: authOnly },
   },
 ];
 
