41f1cac7a1394cc0a9295ccb1b6095ae65d7604c
mohamed_sakr <sakr83117@gmail.com>
2023-10-15 22:53:13 +0300

    Home landing page edits

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,14 +5,25 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 const material = [
   MatInputModule,
   MatToolbarModule,
   MatIconModule,
   MatFormFieldModule,
+  MatListModule,
   MatButtonModule,
   MatSnackBarModule,
+  MatSidenavModule,
+  MatCardModule,
+  MatMenuModule,
+  MatRippleModule,
+  MatDividerModule,
 ];
 
 @NgModule({
